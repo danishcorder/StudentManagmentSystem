324d0035a0102dbac92e6e8c9f24dea65e70621d
HadisaMaryam <hadisamaryam31@gmail.com>
2025-04-22 17:34:44 +0500

    Update studentinfo.py
    Added instructor attribute

--- studentinfo.py
+++ studentinfo.py
@@ -1,14 +1,16 @@
 class Student:
-    def __init__(self, name, roll_number, course):
+    def __init__(self, name, roll_number, course,instructor):
         self.name = name
         self.roll_number = roll_number
         self.course = course
+        self. instructor=instructor
 
     def display_info(self):
         print("----- Student Information -----")
         print(f"Name       : {self.name}")
         print(f"Roll No.   : {self.roll_number}")
         print(f"Course     : {self.course}")
+        print(f"Instructor  :{self.instructor}) 
         print("-------------------------------")
 
 # Example usage
